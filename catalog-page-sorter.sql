0c2e4fdbca9a639690864ccd0ca0dce19dc2c2ba
Object <87041394+ObjectRetros@users.noreply.github.com>
2023-04-10 01:33:40 +0200

    Update catalog-page-sorter.sql

--- catalog-page-sorter.sql
+++ catalog-page-sorter.sql
@@ -1,8 +1,9 @@
 SET @orderNum = 0;
 
 UPDATE catalog_pages
-SET order_num = (@orderNum := @orderNum + 1)
-WHERE parent_id NOT IN ('-1'
+SET order_num = (@orderNum := @orderNum + 1) -1
+WHERE parent_id NOT IN (
+	-1
     -- Your catalog page IDs to exclude from the sorting. Eg. catalog tabs etc. separate them by commas. - Eg. -1,1,2,3,4,5,6
 )
 -- AND parent_id = '1' optional to for example only alter pages under certain category
